abc6f2c7d408f4485c262b3aa2e7ffdf4865cfed
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-09-24 14:23:12 +0900

    version 0.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ URL="https://github.com/KoichiYasuoka/spaCy-Coptic"
 
 setuptools.setup(
   name="spacy_coptic",
-  version="0.1.0",
+  version="0.2.0",
   description="Coptic NLP wrapper for spaCy",
   long_description=long_description,
   long_description_content_type="text/markdown",
