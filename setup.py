95c2b4f28348e281791df98dc0fae8d6979ed3b6
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2022-05-15 08:50:22 +0900

    version 0.7.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ URL="https://github.com/KoichiYasuoka/spaCy-Coptic"
 
 setuptools.setup(
   name="spacy_coptic",
-  version="0.7.4",
+  version="0.7.5",
   description="Coptic NLP wrapper for spaCy",
   long_description=long_description,
   long_description_content_type="text/markdown",
@@ -16,7 +16,7 @@ setuptools.setup(
   license="MIT",
   keywords="Coptic spaCy",
   packages=setuptools.find_packages(),
-  install_requires=["spacy>=2.2.2","deplacy>=1.9.9"],
+  install_requires=["spacy>=2.2.2","deplacy>=2.0.2"],
   python_requires=">=3.6",
   classifiers=[
     "License :: OSI Approved :: MIT License",
